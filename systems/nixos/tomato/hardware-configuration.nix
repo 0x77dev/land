3780e2e68a4761c10cdb7c6cf6b68406afe40c92
Mykhailo Marynenko <mykhailo@0x77.dev>
2024-12-29 19:41:02 -0800

    simplify tomato

--- systems/nixos/tomato/hardware-configuration.nix
+++ systems/nixos/tomato/hardware-configuration.nix
@@ -6,38 +6,31 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  # ZFS
-  boot.supportedFilesystems = [ "zfs" ];
-  boot.zfs.forceImportRoot = false;
-  networking.hostId = "4d878a45";
-  services.zfs.autoScrub.enable = true;
-
   fileSystems."/" =
     {
-      device = "/dev/disk/by-uuid/0964a4ba-24ce-45a5-bf2b-a4aab8080e34";
-      fsType = "ext4";
+      device = "rpool/root";
+      fsType = "zfs";
+    };
+
+  fileSystems."/home" =
+    {
+      device = "rpool/home";
+      fsType = "zfs";
     };
 
   fileSystems."/boot" =
     {
-      device = "/dev/disk/by-uuid/1D52-3890";
+      device = "/dev/disk/by-uuid/1F38-8495";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  fileSystems."/data" =
-    {
-      device = "data";
-      fsType = "zfs";
-    };
-
-  swapDevices =
-    [{ device = "/dev/disk/by-uuid/69ed0d72-8909-44ee-8e66-b47567982cef"; }];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -49,6 +42,7 @@
   # networking.interfaces.enp87s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp90s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp91s0.useDHCP = lib.mkDefault true;
+  networking.hostId = "442cbd39";
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
