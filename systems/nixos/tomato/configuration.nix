819da87bfe7c990fbaae013cfb8f4cb1d37e5d09
Mykhailo Marynenko <mykhailo@0x77.dev>
2024-12-29 21:21:58 -0800

    add avahi to tomato

--- systems/nixos/tomato/configuration.nix
+++ systems/nixos/tomato/configuration.nix
@@ -74,6 +74,19 @@
     rpc.secret = "mykhailo";
   };
 
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
+
   nixpkgs.config.allowUnfree = true;
 
   # Open ports in the firewall.
