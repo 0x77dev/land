c279c09a278bdf0d2bc005e573dcae5cfe5a0173
Mykhailo Marynenko <mykhailo@0x77.dev>
2024-12-29 22:43:09 -0800

    update tomato firewall

--- systems/nixos/tomato/configuration.nix
+++ systems/nixos/tomato/configuration.nix
@@ -96,8 +96,8 @@
   };
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 22 19999 139 445 ];
+  networking.firewall.allowedUDPPorts = [ 137 138 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
