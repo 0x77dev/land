2a8e4899d42c9e74db3109aaffad2ed64dfaa6d8
Mykhailo Marynenko <mykhailo@0x77.dev>
2024-12-30 23:44:58 -0800

    enhance netdata

--- systems/nixos/tomato/security.nix
+++ systems/nixos/tomato/security.nix
@@ -2,9 +2,18 @@
   security.sudo = {
     enable = true;
     wheelNeedsPassword = true;
-    # Allow netdata to run smbstatus command as root
+    # Allow netdata to run monitoring commands as root
     extraConfig = ''
+      # Samba monitoring
       netdata ALL=(root) NOPASSWD: ${pkgs.samba}/bin/smbstatus
+      # Fail2ban monitoring
+      netdata ALL=(root) NOPASSWD: ${pkgs.fail2ban}/bin/fail2ban-client status
+      netdata ALL=(root) NOPASSWD: ${pkgs.fail2ban}/bin/fail2ban-client status *
+      # IPFS monitoring
+      netdata ALL=(root) NOPASSWD: ${pkgs.ipfs}/bin/ipfs stats *
+      # Process monitoring
+      netdata ALL=(root) NOPASSWD: ${pkgs.procps}/bin/ps -C * -O pcpu,pmem,comm
+      netdata ALL=(root) NOPASSWD: ${pkgs.iproute2}/bin/ss -tupl
     '';
   };
 }
--- systems/nixos/tomato/services/monitoring.nix
+++ systems/nixos/tomato/services/monitoring.nix
@@ -7,16 +7,79 @@
     };
     configDir."python.d.conf" = pkgs.writeText "python.d.conf" ''
       samba: yes
+      postgres: yes
+      redis: yes
+      fail2ban: yes
+    '';
+    configDir."go.d.conf" = pkgs.writeText "go.d.conf" ''
+      clickhouse: yes
+      ipfs: yes
+    '';
+    config = {
+      # NOTE: postgres netdata user must be created and have pg_monitor privileges
+      # CREATE USER netdata;
+      # GRANT pg_monitor TO netdata;
+      "plugin:postgres" = {
+        "update every" = 10;
+        "command options" = "host=/run/postgresql";
+      };
+      "plugin:discovery" = {
+        "enable running new plugins" = "yes";
+        "check for new plugins every" = 60;
+      };
+      "plugin:apps" = {
+        "update every" = 5;
+        "command options" = "without-users";
+      };
+      "plugin:proc" = {
+        "command options" = "group services=systemd-services";
+      };
+      "plugin:fail2ban" = {
+        "update every" = 10;
+        "log file" = "/var/log/fail2ban.log";
+      };
+    };
+    configDir."go.d/ipfs.conf" = pkgs.writeText "ipfs.conf" ''
+      jobs:
+        - name: local
+          url: http://localhost:5001
     '';
   };
 
   # Configure netdata service
   systemd.services.netdata = {
-    # Add samba and sudo to path for python plugin
-    path = [ pkgs.samba "/run/wrappers" ];
+    path = [
+      pkgs.samba
+      pkgs.postgresql
+      pkgs.fail2ban
+      pkgs.ipfs
+      pkgs.procps
+      pkgs.iproute2
+      "/run/wrappers"
+    ];
 
-    # Set minimal capabilities needed for samba monitoring
-    serviceConfig.CapabilityBoundingSet = [ "CAP_SETGID" ];
+    serviceConfig = {
+      CapabilityBoundingSet = [
+        "CAP_SETGID"
+        "CAP_DAC_READ_SEARCH" # For reading system files
+        "CAP_SYS_PTRACE" # For process monitoring
+        "CAP_NET_RAW" # For network monitoring
+      ];
+      SupplementaryGroups = [
+        "postgres"
+        "redis"
+        "systemd-journal"
+        "proc"
+      ];
+      ReadWritePaths = [
+        "/var/log/fail2ban.log"
+        "/var/cache/netdata"
+        "/var/lib/netdata"
+      ];
+      ProtectSystem = "strict";
+      ProtectHome = true;
+      NoNewPrivileges = false; # Required for sudo access
+    };
   };
 
   services.iperf3 = {
