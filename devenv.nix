dcea45560c7a4953899a11dc0752eded26b7f6c3
Mykhailo Marynenko <mykhailo@0x77.dev>
2024-12-31 20:46:53 -0800

    add actionlint

--- devenv.nix
+++ devenv.nix
@@ -14,6 +14,7 @@
   cachix.pull = [ "land" "devenv" ];
   cachix.push = "land";
 
+  pre-commit.hooks.actionlint.enable = true;
   pre-commit.hooks.shellcheck.enable = true;
   pre-commit.hooks.nixpkgs-fmt.enable = true;
   pre-commit.hooks.mdsh.enable = true;
