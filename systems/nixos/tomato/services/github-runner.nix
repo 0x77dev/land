4eb1a79df778e1180b0ea4bdc7f4832cdc9f036c
Mykhailo Marynenko <mykhailo@0x77.dev>
2025-01-04 12:55:25 -0800

    fix github-runner on tomato

--- systems/nixos/tomato/services/github-runner.nix
+++ systems/nixos/tomato/services/github-runner.nix
@@ -1,14 +1,12 @@
 { ... }: {
-  services.github-runner = {
-    land = {
-      enable = true;
-      url = "https://github.com/0x77dev/land";
-      name = "tomato";
-      tokenFile = "/run/secrets/github-runner/token";
-      user = "github-runner";
-      group = "github-runner";
-      replace = true;
-      extraLabels = [ "home-lab" ];
-    };
+  services.github-runners.land = {
+    enable = true;
+    url = "https://github.com/0x77dev/land";
+    name = "tomato";
+    tokenFile = "/run/secrets/github-runner/token";
+    user = "github-runner";
+    group = "github-runner";
+    replace = true;
+    extraLabels = [ "home-lab" ];
   };
 }
