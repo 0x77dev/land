7fe60f423e3dc3f260260103d382eae4422841e6
Mykhailo Marynenko <mykhailo@0x77.dev>
2025-11-23 16:20:14 -0800

    Sun Nov 23 16:20:14 PST 2025

--- nix/systems/aarch64-darwin/beefy/default.nix
+++ nix/systems/aarch64-darwin/beefy/default.nix
@@ -27,23 +27,25 @@ in
       path = "/Users/${userName}";
 
       config = {
-        ai.enable = true;
-        cloud.enable = true;
-        fonts.enable = true;
-        ghostty.enable = true;
-        git.enable = true;
-        ide.enable = true;
-        media.enable = true;
-        mobile.enable = true;
-        network.enable = true;
-        nix.enable = true;
-        p2p.enable = true;
-        reverse-engineering.enable = true;
-        comms.enable = true;
-        security.enable = true;
-        shell.enable = true;
-        ssh.enable = true;
-        gpg.enable = true;
+        modules.home = {
+          ai.enable = true;
+          cloud.enable = true;
+          fonts.enable = true;
+          ghostty.enable = true;
+          git.enable = true;
+          ide.enable = true;
+          media.enable = true;
+          mobile.enable = true;
+          network.enable = true;
+          nix.enable = true;
+          p2p.enable = true;
+          reverse-engineering.enable = true;
+          comms.enable = true;
+          security.enable = true;
+          shell.enable = true;
+          ssh.enable = true;
+          gpg.enable = true;
+        };
       };
     };
   };
