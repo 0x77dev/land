b73b4e95a3042fd967ac532bb4c69ac394218577
Mykhailo Marynenko <mykhailo@0x77.dev>
2025-12-01 18:11:18 -0800

    Mon Dec  1 06:11:18 PM PST 2025

--- nix/lib/shared/ntp-config/default.nix
+++ nix/lib/shared/ntp-config/default.nix
@@ -1,50 +1,10 @@
 _: {
-  # Default NTP servers with standard leap second handling (no smearing).
-  # Safe to use together and with GPS/PPS time sources.
-  #
-  # EXCLUDED servers (leap smearing incompatible with standard NTP/GPS):
-  # - time.google.com - smears since 2008
-  # - time.aws.com - smears
-  # - time.facebook.com - smears
-  # - amazon.pool.ntp.org - NOT run by Amazon, just a pool.ntp.org vendor zone
+  # NTP servers with standard leap second handling (no smearing).
+  # Excludes: time.google.com, time.aws.com, time.facebook.com (all smear)
   defaultServers = [
-    # Cloudflare - explicitly documented as non-smearing
-    # https://developers.cloudflare.com/time-services/ntp/
-    "time.cloudflare.com"
-
-    # NIST - US government time standard (stratum 1)
-    "time.nist.gov"
-    "time-a-g.nist.gov"
-    "time-b-g.nist.gov"
-
-    # Internet Systems Consortium
-    "clock.isc.org"
-
-    # Netnod Sweden - stratum 1, anycast
-    "ntp.se"
-
-    # TimeNL/SIDN Labs - Dutch stratum 1, NTS support
-    "ntp.time.nl"
-
-    # RIPE NCC
-    "ntp.ripe.net"
-
-    # Hurricane Electric - stratum 1
-    "clock.sjc.he.net"
-    "clock.nyc.he.net"
-
-    # NICT Japan - National Institute of Information and Communications Technology
-    "ntp.nict.jp"
-
-    # Austrian Academic Computer Network
-    "ts1.aco.net"
-    "ts2.aco.net"
-
-    # Hetzner - European hosting provider
-    "ntp1.hetzner.de"
-    "ntp2.hetzner.de"
-
-    # NTP Pool - distributed, standard behavior (no smearing allowed)
-    "pool.ntp.org"
+    "time.cloudflare.com" # Global anycast, documented non-smearing
+    "time.nist.gov" # US government stratum 1
+    "ntp.se" # Netnod Sweden, stratum 1 anycast
+    "pool.ntp.org" # Distributed fallback
   ];
 }
