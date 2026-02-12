4fa9795bf3b00d8628110b08833cdccf27953458
Mykhailo Marynenko <mykhailo@0x77.dev>
2026-02-11 17:38:09 -0800

    Wed Feb 11 17:38:09 PST 2026

--- nix/homes/aarch64-darwin/mykhailo@beefy/default.nix
+++ nix/homes/aarch64-darwin/mykhailo@beefy/default.nix
@@ -13,15 +13,14 @@ in
     # Use local LM Studio for openclaw on beefy
     openclaw = {
       config = {
-        plugins = {
-          telegram.enable = true;
-          # oracle.enable = true;
-          # sag.enable = true;
-          # gogcli.enable = true;
-          # goplaces.enable = true;
-          # peekaboo.enable = true;
-          # poltergeist.enable = true;
-          # imsg.enable = true;
+        gateway = {
+          mode = "local";
+          bind = "loopback";
+          tailscale.mode = "serve";
+          auth = {
+            token = "\${OPENCLAW_GATEWAY_TOKEN}";
+            allowTailscale = true;
+          };
         };
       };
       instances.default = {
