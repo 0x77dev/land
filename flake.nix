b197102892b53e8da3cf5683bf79eb477e83ef9d
Mykhailo Marynenko <mykhailo@0x77.dev>
2026-07-09 17:08:47 -0400

    chore(flake): substitute from land.cachix.org

--- flake.nix
+++ flake.nix
@@ -101,12 +101,14 @@
   nixConfig = {
     extra-substituters = [
       "https://cache.nixos.org"
+      "https://land.cachix.org"
       "https://nix-community.cachix.org"
       "https://nixos-raspberrypi.cachix.org"
       "https://vicinae.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "land.cachix.org-1:9KPti8Xi0UJ7eQof7b8VUzSYU5piFy6WVQ8MDTLOqEA="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
       "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
